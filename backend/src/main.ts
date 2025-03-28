5d01bf63795a59a2f1c731d61b5171e384fa253a
zwisler <alex-z-@web.de>
2025-03-28 10:08:41 +0100

    Sorry, needed for testing

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { json, urlencoded } from 'express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: { origin: '*' } });
   app.use(json({ limit: '5000mb' }));
 
   const config = new DocumentBuilder().setTitle('El erizo').setDescription('El erizo').setVersion('1.0').build();
