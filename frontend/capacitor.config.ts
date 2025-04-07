07c085d94932e43d70438bb6a6d84b8d5c557ade
Alex <az11kyho@studserv.uni-leipzig.de>
2025-04-07 22:43:58 +0200

    Apppppps

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'de.zwisler.erizo',
+  appName: 'erizo',
+  webDir: 'dist'
+};
+
+export default config;
