2b6ba75631c994dde5863f2b4e14821e21934cfb
Alex <az11kyho@studserv.uni-leipzig.de>
2025-04-07 23:02:38 +0200

    Sync

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'de.zwisler.erizo',
   appName: 'erizo',
-  webDir: 'dist'
+  webDir: 'dist/frontend/browser'
 };
 
 export default config;
