7afd611ea5a6ea00460688a807882a82777212f8
Alex <az11kyho@studserv.uni-leipzig.de>
2025-04-25 21:39:44 +0200

    Style adjustments for apps

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {SafeArea} from 'capacitor-plugin-safe-area';
+
+SafeArea.addListener('safeAreaChanged', data => {
+  const { insets } = data;
+  console.log('safeAreaChanged', insets);
+  for (let [key, value] of Object.entries(insets)) {
+    if(key === 'top') value = Math.max(value, 8);
+    document.documentElement.style.setProperty(
+      `--safe-area-inset-${key}`,
+      `${value}px`,
+    );
+  }
+});
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
