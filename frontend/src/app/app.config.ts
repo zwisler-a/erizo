64c453cf6eff9e06a102da205434c3ed9160dad0
zwisler <alex-z-@web.de>
2025-08-06 10:57:32 +0200

    feat: fix url

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -21,7 +21,7 @@ import {LoadingInterceptor} from './core/interceptors/loading.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(ApiModule.forRoot({rootUrl: localStorage.getItem('url') || 'https://192.168.178.179:4200'})),
+    importProvidersFrom(ApiModule.forRoot({rootUrl: localStorage.getItem('url') || 'https://erizo.zwisler.dev'})),
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
     provideAnimationsAsync(),
